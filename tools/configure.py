d7023fda3d021ca169b2bc6ffa02771330103e45
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-12-22 11:05:03 +0800

    feat: ocr model

---
+++ tools/configure.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import shutil
+
+assets_dir = Path(__file__).parent.parent
+
+
+def configure_ocr_model():
+    shutil.copytree(
+        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
+        assets_dir / "resource" / "model" / "ocr",
+        dirs_exist_ok=True,
+    )
+
+
+if __name__ == "__main__":
+    configure_ocr_model()
+    print("OCR model configured.")
